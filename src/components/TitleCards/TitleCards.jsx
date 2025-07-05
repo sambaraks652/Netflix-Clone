1f8adeae71b6d428edcb7544b117aeec7da6ed74
sambaraks652 <sambaraks652@gmail.com>
2025-07-05 20:25:23 +0300

    added some functionalities

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
+  const cardsRef = useRef();
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
   return (
-    <div className="titlecards">
+    <div className="title-cards">
       <h2>Popular on Netflix</h2>
-      <div className="card-list">
+      <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => {
           return (
             <div className="card" key={index}>
