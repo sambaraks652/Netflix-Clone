d7e7c6ffb3112cc18c7e0cb4346d06a89f3f519c
sambaraks652 <sambaraks652@gmail.com>
2025-07-19 21:28:49 +0300

    changed some things

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,9 +13,9 @@ const Home = () => {
     <div className="home">
       <Navbar />
       <div className="hero">
-        <img src={hero_banner} alt="" className="banner-img" />
+        <img src={hero_banner} alt="banner" className="banner-img" />
         <div className="hero-caption">
-          <img src={hero_title} alt="" className="caption-img" />
+          <img src={hero_title} alt="caption" className="caption-img" />
           <p>
             Discovering his ties to a secret ancient order, a young man living
             in modern Istanbul embarks on a quest to save the city from an
@@ -23,12 +23,10 @@ const Home = () => {
           </p>
           <div className="hero-btns">
             <button className="btn">
-              {" "}
-              <img src={play_icon} alt="" /> Play
+              <img src={play_icon} alt="play" /> Play
             </button>
             <button className="btn dark-btn">
-              {" "}
-              <img src={info_icon} alt="" /> More Info
+              <img src={info_icon} alt="info" /> More Info
             </button>
           </div>
         </div>
